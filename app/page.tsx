e16d34629a861f4ea317b737f19e61f25807b744
LucasSalaun <lucas07salaun@gmail.com>
2026-04-30 01:39:15 +0200

    1er soirée un peu de css avec un peut de logique

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,78 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [money, setMoney] = useState(100_000_000);
+  const [rolling, setRolling] = useState(false);
+  const [result2, setResult2] = useState("?");
+  const playSoundNormal = () => {
+    const normal = new Audio("/dice_sound_normal.m4a");
+    normal.volume = 0.5;
+    normal.play();
+  };
+  const playSoundWin = () => {
+    const win = new Audio("/dice_sound_win.m4a");
+    win.volume = 0.5;
+    win.play();
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main>
+      <h1>Archfiend Dice Simulator</h1>
+      <div className="money">{money.toLocaleString()} coins</div>
+      <h2>You rolled {result2}</h2>
+      <img src="/dice.webp" alt="dice" width={150} className="dice" />
+      <button
+        className="roll-button"
+        disabled={rolling}
+        onClick={() => {
+          if (money < 6_600_000) {
+            alert("Not enough money to roll the dice!");
+            return;
+          }
+
+          setRolling(true);
+
+          const result = rollDice();
+          setResult2("?");
+          if ([1, 2, 3, 4, 5].includes(result)) {
+            playSoundNormal();
+          }
+          if ([6, 7].includes(result)) {
+            playSoundWin();
+          }
+          setTimeout(() => {
+            setResult2(result.toLocaleString());
+            setMoney((prev) => prev - 6_600_000);
+
+            if (result === 6) {
+              setMoney((prev) => prev + 100_000_000);
+            }
+
+            if (result === 7) {
+              alert(
+                "Congratulations! You rolled a 7 and won an Archfiend Dye! You can sell it for 37 million coins",
+              );
+            }
+
+            setRolling(false);
+          }, 2500);
+        }}
+      >
+        {rolling ? "Rolling..." : "Roll Dice"}
+      </button>
+    </main>
   );
+}
+
+function rollDice() {
+  const rand = Math.random() * 100;
+
+  if (rand < 18.72) return 1;
+  if (rand < 37.44) return 2;
+  if (rand < 56.16) return 3;
+  if (rand < 74.88) return 4;
+  if (rand < 93.6) return 5;
+  if (rand < 99.84) return 6;
+  return 7;
 }
