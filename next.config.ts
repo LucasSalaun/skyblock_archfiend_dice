37298141dfc284e90f086fc64cf6b5501f6f630e
Lucas Salaun <lucas07salaun@gmail.com>
2026-05-04 13:51:19 +0200

    Modify next.config.ts for GitHub Pages support
    Updated Next.js configuration for GitHub Pages deployment.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Génère un dossier 'out' au lieu d'un build classique
+  images: {
+    unoptimized: true, // Requis car GitHub Pages ne supporte pas l'optimisation d'image native de Next.js
+  },
+  // À ajouter UNIQUEMENT si ton site est déployé sur une URL du type username.github.io/nom-du-repo/
+  // basePath: '/nom-du-repo', 
 };
 
 export default nextConfig;
