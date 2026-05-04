5415e0c59cd018a55bdb36c2dbe9d617ef6536b7
Lucas Salaun <lucas07salaun@gmail.com>
2026-05-04 13:56:40 +0200

    Update basePath in next.config.ts
    Set basePath for deployment on GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig = {
     unoptimized: true, // Requis car GitHub Pages ne supporte pas l'optimisation d'image native de Next.js
   },
   // À ajouter UNIQUEMENT si ton site est déployé sur une URL du type username.github.io/nom-du-repo/
-  // basePath: '/nom-du-repo', 
+  basePath: '/skyblock_archfiend_dice', 
 };
 
 export default nextConfig;
